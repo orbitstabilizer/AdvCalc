a891a0ebace58a287118407d214f0900d128ebe7
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-03-14 00:25:36 +0300

    Update.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "../include/parser.h"
+#include "../include/executor.h"
 
 
 #define BUFFER_SIZE 256
@@ -10,9 +12,19 @@ int main() {
   char buffer[BUFFER_SIZE];
   while (true) {
     printf("> ");
+    // TODO: Fix buffer overflow
     if(fgets(buffer, BUFFER_SIZE, stdin) == NULL)
       break;
     printf("You entered: %s\n", buffer); 
+    
+    // returns a pointer to the first token
+    Token* parse_tree = parse(buffer);
+      
+    char* output = eval(parse_tree);
+    
+    if (output){
+      printf("%s\n", output);
+    }
   }
   return 0;
 
