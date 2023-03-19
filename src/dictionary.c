c3c8027c7a805431751ef489f5fd5a95b98311b8
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-03-19 04:47:08 +0300

    Dictionary api is ready to use

--- src/dictionary.c
+++ src/dictionary.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/dictionary.h"
-struct nlist { /* table entry: */
-    struct nlist *next; /* next entry in chain */
-    char *name; /* defined name */
-    char *defn; /* replacement text */
-};
 
-#define HASHSIZE 101
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+Dictionary* new_dictionary(){
+  Dictionary* dict = (Dictionary*) malloc(sizeof(Dictionary));
+  return dict;
+}
 
 /* hash: form hash value for string s */
 unsigned hash(char *s)
@@ -21,61 +18,125 @@ unsigned hash(char *s)
 }
 
 /* lookup: look for s in hashtab */
-struct nlist *lookup(char *s)
+Chain *get(Dictionary* dict, char *s)
 {
-    struct nlist *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+    Chain *np;
+    for (np = dict->hashtab[hash(s)]; np != NULL; np = np->next)
         if (strcmp(s, np->name) == 0)
           return np; /* found */
     return NULL; /* not found */
 }
 
-char *strdup(char *);
-/* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *defn)
+
+Chain *put(Dictionary* dict, char *name, long value)
 {
-    struct nlist *np;
+    Chain *np;
     unsigned hashval;
-    if ((np = lookup(name)) == NULL) { /* not found */
-        np = (struct nlist *) malloc(sizeof(*np));
+    if ((np = get(dict, name)) == NULL) { /* not found */
+        np = (Chain *) malloc(sizeof(*np));
         if (np == NULL || (np->name = strdup(name)) == NULL)
           return NULL;
         hashval = hash(name);
-        np->next = hashtab[hashval];
-        hashtab[hashval] = np;
-    } else /* already there */
-        free((void *) np->defn); /*free previous defn */
-    if ((np->defn = strdup(defn)) == NULL)
-       return NULL;
+        np->next = dict->hashtab[hashval];
+        dict->hashtab[hashval] = np;
+    } 
+    np->value = value;
     return np;
 }
 
-char *strdup(char *s) /* make a duplicate of s */
-{
-    char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
-    if (p != NULL)
-       strcpy(p, s);
-    return p;
+void print_dict(Dictionary* dict){
+  Chain* bin;
+  for (int i = 0; i < HASHSIZE; i++){
+    bin = dict->hashtab[i];
+    if (bin != NULL){
+      printf("bin %d: ", i);
+      while (bin != NULL){
+        printf("%s = %ld, ", bin->name, bin->value);
+        bin = bin->next;
+      }
+      printf("\n");
+    }
+  }
 }
 
-void declare_var(char *name){
-  setenv(name, "0", 1); 
+void free_dict(Dictionary* dict){
+  Chain* bin;
+  Chain* next;
+  for (int i = 0; i < HASHSIZE; i++){
+    bin = dict->hashtab[i];
+    while (bin != NULL){
+      next = bin->next;
+      free(bin->name);
+      free(bin);
+      bin = next;
+    }
+  }
+  free(dict);
 }
 
-void set_var(char *name, long value){
-  // int to string
-  char value_str[10];
-  sprintf(value_str, "%ld", value);
-  setenv(name, value_str, 1);
+void set_var(Dictionary *dict, char *name, long value){
+  put(dict, name, value);
 }
 
-long get_var(char *name){
-  char* value_str = getenv(name);
-  if(value_str == NULL){
+long get_var(Dictionary *dict, char *name){
+  Chain* bin = get(dict, name);
+  if(bin == NULL){
     // if varialbe is not declared 
     return 0;
   }
-  return atol(value_str);
+  return bin->value;
 }
 
+/*
+int main(){
+  // put("alis", "1");
+  Dictionary* dict = new_dictionary();
+  set_var(dict, "alis", 31);
+  // put("b", "2");
+  set_var(dict, "aD", 2);
+  printf("alis = %ld\n", get_var(dict, "alis"));
+  set_var(dict,  "alis", 54);
+  printf("alis = %ld\n", get_var(dict, "alis"));
+  printf("aD = %ld\n", get_var(dict, "aD"));
+  printf("var = %ld\n", get_var(dict, "var"));
+  set_var(dict, "one", 1);  
+  set_var(dict, "two", 2);
+  set_var(dict, "three", 3);
+  set_var(dict, "four", 4);
+  set_var(dict, "five", 5);
+  set_var(dict, "six", 6);
+  set_var(dict, "seven", 7);
+  set_var(dict, "eight", 8);
+  set_var(dict, "nine", 9);
+  set_var(dict, "ten", 10);
+  set_var(dict, "eleven", 11);
+  set_var(dict, "twelve", 12);
+  set_var(dict, "thirteen", 13);
+  set_var(dict, "fourteen", 14);
+  set_var(dict, "fifteen", 15);
+  set_var(dict, "sixteen", 16);
+  set_var(dict, "seventeen", 17);
+  set_var(dict, "eighteen", 18);
+  set_var(dict, "nineteen", 19);
+  set_var(dict, "twenty", 20);
+  set_var(dict, "twentyone", 21);
+  set_var(dict, "twentytwo", 22);
+  set_var(dict, "twentythree", 23);
+  set_var(dict, "twentyfour", 24);
+  set_var(dict, "twentyfive", 25);
+  set_var(dict, "twentysix", 26);
+  set_var(dict, "twentyseven", 27);
+  set_var(dict, "twentyeight", 28);
+  set_var(dict, "twentynine", 29);
+  set_var(dict, "thirty", 30);
+  set_var(dict, "thirtyone", 31);
+  
+  // print_table();
+  printf("twentytwo = %ld\n", get_var(dict, "twentytwo"));
+  printf("twentynine = %ld\n", get_var(dict, "twentynine"));
+  print_dict(dict);
+  free_dict(dict);
+  return 0;
+
+  }
+*/
