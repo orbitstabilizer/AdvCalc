fd931e4d2f3f799061b9c2683996bb194a3ee3e1
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-03-21 01:15:46 +0300

    Refactored the program flow

--- include/executor.h
+++ include/executor.h
@@ -4,18 +4,18 @@
 #include "../include/pro_parser.h"
 #include "../include/dictionary.h"
 
-char *exec(char *input, Dictionary *dict, bool *error);
+int exec(Dictionary *dict, char *input, char *output);
 long str_to_long(char *str);
 long add(long a, long b);
 long sub(long a, long b);
 long mul(long a, long b);
-long and (long a, long b);
-long or (long a, long b);
-long xor (long a, long b);
+long b_and (long a, long b);
+long b_or (long a, long b);
+long b_xor (long a, long b);
 long lshift(long a, long b);
 long rshift(long a, long b);
 long lrotate(long a, long b);
 long rrotate(long a, long b);
-long not(long a, long null);
+long b_not(long a, long null);
 
 #endif
--- src/executor.c
+++ src/executor.c
@@ -1,17 +1,16 @@
 
-#include "../include/pro_parser.h"
-#include "../include/debug.h"
 #include "../include/executor.h"
+#include "../include/debug.h"
 #include "../include/dictionary.h"
 #include "../include/lexer.h"
+#include "../include/pro_parser.h"
 #undef DEBUG
-#define DEBUG 1
+#define DEBUG 0
 
 #define RAISE_ERROR                                                            \
   do {                                                                         \
     goto error;                                                                \
   } while (0)
-
 
 typedef long (*op_t)(long, long);
 op_t token_to_op(TokenType type) {
@@ -23,11 +22,11 @@ op_t token_to_op(TokenType type) {
   case TOKEN_MINUS:
     return sub;
   case TOKEN_AND:
-    return and;
+    return b_and;
   case TOKEN_OR:
-    return or ;
+    return b_or;
   case TOKEN_XOR:
-    return xor;
+    return b_xor;
   case TOKEN_LS:
     return lshift;
   case TOKEN_RS:
@@ -37,7 +36,7 @@ op_t token_to_op(TokenType type) {
   case TOKEN_RR:
     return rrotate;
   case TOKEN_NOT:
-    return not ;
+    return b_not;
   default:
     return NULL;
   }
@@ -46,11 +45,10 @@ op_t token_to_op(TokenType type) {
 long add(long a, long b) { return a + b; }
 long sub(long a, long b) { return a - b; }
 long mul(long a, long b) { return a * b; }
-long and (long a, long b) { return a & b; }
-long or (long a, long b) { return a | b; }
-long xor (long a, long b) { return a ^ b; } long lshift(long a, long b) {
-  return a << b;
-}
+long b_and(long a, long b) { return a & b; }
+long b_or(long a, long b) { return a | b; }
+long b_xor(long a, long b) { return a ^ b; }
+long lshift(long a, long b) { return a << b; }
 long rshift(long a, long b) { return a >> b; }
 long lrotate(long a, long b) {
   return (a << b) | (a >> (sizeof(long) * 8 - b));
@@ -58,7 +56,7 @@ long lrotate(long a, long b) {
 long rrotate(long a, long b) {
   return (a >> b) | (a << (sizeof(long) * 8 - b));
 }
-long not(long a, long null) {
+long b_not(long a, long null) {
   // to resolve unused parameter warning
   null = 0;
   a = a + null;
@@ -77,23 +75,22 @@ long str_to_long(char *str) {
   }
   return atol(str);
 }
-bool error = false;
 
-long eval_util(SyntaxNode *parent, Dictionary *dict) {
-  if (error || parent == NULL || parent->type == ERROR)
+long eval_util(SyntaxNode *parent, Dictionary *dict, bool *error) {
+  if (*error || parent == NULL || parent->type == ERROR)
     RAISE_ERROR;
   long left = 0;
   long right = 0;
   if (parent->type == BINOP || parent->type == UNOP || parent->type == PAREN)
-    left = eval_util(parent->left, dict);
+    left = eval_util(parent->left, dict, error);
   if (parent->type == BINOP)
-    right = eval_util(parent->right, dict);
+    right = eval_util(parent->right, dict, error);
   if (parent->type == TOKEN) {
     if (parent->token->type == TOKEN_LITERAL) {
       return parent->token->value;
     } else if (parent->token->type == TOKEN_IDENTIFIER) {
       size_t len = parent->token->length;
-      char *var = (char*)malloc(sizeof(char) * (len + 1));
+      char *var = (char *)malloc(sizeof(char) * (len + 1));
       strncpy(var, parent->token->start, len);
       var[len] = '\0';
       long val = get_var(dict, var);
@@ -112,72 +109,64 @@ long eval_util(SyntaxNode *parent, Dictionary *dict) {
   }
   return 0;
 error:
-  error = true;
+  *error = true;
   return 0;
 }
-#define RAISE_EXEC_ERROR                                                           \
-  do {                                                                         \
-    goto execution_error;                                                                \
-  } while (0)
 
-char* exec(char *input, Dictionary *dict, bool *err) {
-  error = false;
 
-  char *output = NULL;
+int exec(Dictionary *dict, char *input, char *output) {
   size_t input_len = strlen(input);
   Lexer *lexer = lexer_new(input, input_len);
   SyntaxTree *tree = NULL;
   TokenType type = TOKEN_EOF;
-  char *assign = NULL;
+  bool is_assignment = false;
+  char *variable = NULL;
+  int state = 0;
   do {
     lexer_next(lexer);
     type = lexer->token_list[lexer->cur_token - 1].type;
-    if (type == TOKEN_UNKNOWN) {
-      if(DEBUG)print_lex(lexer); 
-      error = true;
-      RAISE_EXEC_ERROR;
+    if (type == TOKEN_EOF && lexer->cur_token == 1){
+      state = 1;
+      goto end;
     }
-    if (lexer->cur_token == 2 && type == TOKEN_EQ){
-      if (lexer->token_list[0].type != TOKEN_IDENTIFIER) {
-        error = true;
-        RAISE_EXEC_ERROR;
-      }
-      size_t len = lexer->token_list[0].length;
-      assign = (char*) malloc(sizeof(char) * (len + 1));
-      strncpy(assign, lexer->token_list[0].start, len);
-      assign[len] = '\0';
+    if (lexer->cur_token == 2 && type == TOKEN_EQ && lexer->token_list[0].type == TOKEN_IDENTIFIER ) {
+      variable = strndup(lexer->token_list[0].start, lexer->token_list[0].length);
+      is_assignment = true;
+    }    
+    if (type == TOKEN_UNKNOWN || (type == TOKEN_EQ && !is_assignment)) {
+      goto execution_error; 
     }
   } while (type != TOKEN_EOF);
-  if (assign)
-     tree = parse(lexer->token_list + 2);
+
+  if (is_assignment)
+    tree = parse(lexer->token_list + 2);
   else
-    tree = parse(lexer->token_list );
-  long res = eval_util(tree->root, dict);
-  bool empty = false;
-  if (tree->root == NULL && !assign) {
-    error = false;
-    empty = true;
-  }
-  if (error) 
-    RAISE_EXEC_ERROR;
-  else {
-	print_lex(lexer);
-    if (assign) {
-      set_var(dict, assign, res);
-    }
-    else if (!empty){
-      output = (char*)malloc(sizeof(char) * 20);
-      snprintf(output, 20, "%ld", res);
-      }
+    tree = parse(lexer->token_list);
 
-  }
+  bool eval_err = false;
+  long res = eval_util(tree->root, dict, &eval_err);
+  if (eval_err)
+    goto execution_error;
+  else if (is_assignment)
+    set_var(dict, variable, res);
+  snprintf(output, 25, "%ld", res);
+  goto end;
+
 execution_error:
-  if(DEBUG && tree)print_syntax_tree(tree);
-  if(tree)free_syntax_tree(tree);
-  if(lexer)lexer_free(lexer);
-  if(assign)free(assign);
-  *err = error;
-  return output;
+  state = 2;
+end:
+  if (DEBUG && tree){
+    print_lex(lexer);
+    print_syntax_tree(tree);
+  }
+  if (tree)
+    free_syntax_tree(tree);
+  if (lexer)
+    lexer_free(lexer);
+  if (is_assignment && state != 2){
+    state = 3;
+  }
+  if (variable)
+    free(variable);
+  return state;
 }
-
-
